d9c35435fdcc6363bce29c926343e1e4c1de6971
Jenin <jenin8282@gmail.com>
2024-08-24 09:39:24 +0530

    feat: path alias added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-// https://vitejs.dev/config/
+// For ESM (ES Modules)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@pages": resolve(__dirname, "src/pages"),
+      "@components": resolve(__dirname, "src/components"),
+      "@layouts": resolve(__dirname, "src/layouts"),
+      "@hooks": resolve(__dirname, "src/hooks"),
+      "@services": resolve(__dirname, "src/services"),
+      "@utils": resolve(__dirname, "src/utils"),
+      "@context": resolve(__dirname, "src/context"),
+      "@types": resolve(__dirname, "types"),
+      "@": resolve(__dirname, "src"),
+    },
+  },
 });
